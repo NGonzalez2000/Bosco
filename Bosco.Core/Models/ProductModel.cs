3a86eb9d18df90b328539ef697ccc04f2e887296
NGonzalez2000 <nico1_a_gonzalez@hotmail.com>
2022-12-16 12:28:18 -0300

    Add project files.

---
+++ Bosco.Core/Models/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Bosco.Core.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public decimal ListingPrice { get; set; }
+        public decimal RetailPrice { get; set; }
+        public decimal WholesalerPrice { get; set; }
+        public int Stock { get; set; }
+    }
+}
